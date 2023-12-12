12dd582e06b27ae0c9c3725795d19b24f20fc9ba
Codux <codux@codux.com>
2023-12-11 21:19:25 -0500

    Basic Setup

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -1,7 +1,15 @@
+import '../../../index.css';
 import { createBoard } from '@wixc3/react-board';
 import App from '../../../App';
+import { Menu } from '../../../components/menu/menu';
+import { Container } from '../../../components/container/container';
+import { Favorite } from '../../../components/favorite/favorite';
 
 export default createBoard({
     name: 'App',
-    Board: () => <App />,
+    Board: () => <div className="App">
+        <Menu />
+        <Container />
+        <Favorite />
+    </div>,
 });
---
+++ src/_codux/boards/favorite/favorite.board.tsx
@@ -0,0 +1,8 @@
+import { createBoard } from '@wixc3/react-board';
+import { Favorite } from '../../../components/favorite/favorite';
+
+export default createBoard({
+    name: 'Favorite',
+    Board: () => <Favorite />,
+    isSnippet: true,
+});

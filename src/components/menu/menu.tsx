2e358e2ca9d2fd8ccab26b2c36cb3c3763c881c7
Codux <codux@codux.com>
2023-12-13 09:36:37 -0500

    sort by is done

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -5,10 +5,21 @@ export interface MenuProps {
     className?: string;
 }
 
-/**
- * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
- */
+const sortBy: string[] = ["Popularity", "Most Voted", "Release Date"];
+
 export const Menu = ({ className }: MenuProps) => {
-    return <div className={classNames(styles.root, className)}>Menu</div>;
+    return (
+        <div className={classNames(styles.root, className)}>
+            <div className={styles.logo}>
+                <img src="https://play-lh.googleusercontent.com/HLpYdMw9wjDOSw-G3EjdS9IQ1kM3VydWtLZPqSurEWR7KBr0YQW6Qu6ceKvXBT0wPUU=w480-h960" className={styles.logoImg} />
+            </div>
+            <span className={styles.title}>Sort By</span>
+            <hr className={styles.hr} />
+            <ul className={styles.list}>
+                {sortBy.map(item => (
+                    <li className={styles.listItem} key={item}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
